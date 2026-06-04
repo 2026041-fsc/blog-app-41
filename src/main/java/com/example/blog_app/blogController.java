69c053e8bf4b99e028ee9c01ea1e4ddc76f493cb
Esaki <2026041@fsc-educ.com>
2026-06-04 16:31:11 +0900

    ブログの表示確認

--- src/main/java/com/example/blog_app/blogController.java
+++ src/main/java/com/example/blog_app/blogController.java
@@ -3,10 +3,17 @@ package com.example.blog_app;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class blogController {
     @GetMapping("/blogs")
     public String blogs(){
         return "blogs";
     }
+
+    @GetMapping("/blogs/new")
+    public String newBlogs() {
+        return "blogs/new";
+    }
+    
 }
